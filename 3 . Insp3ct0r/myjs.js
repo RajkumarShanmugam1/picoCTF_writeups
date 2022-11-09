af6667cbf98cf0d6bb8afabc73730a34237f7dd2
Rajkumar S <76644058+Sriraj151@users.noreply.github.com>
2022-11-09 18:44:22 +0530

    Insp3ctor

---
+++ 3 . Insp3ct0r/myjs.js
@@ -0,0 +1,21 @@
+function openTab(tabName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+	tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    if(elmnt.style != null) {
+	elmnt.style.backgroundColor = color;
+    }
+}
+
+window.onload = function() {
+    openTab('tabintro', this, '#222');
+}
+
+/* Javascript sure is neat. Anyways part 3/3 of the flag: _lucky?2e7b23e3} */
